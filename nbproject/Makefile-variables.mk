2f0ba19c52bed83de84d65de8f8863693834a71d
Nicolò Vescera <slayercandy@icloud.com>
2015-04-19 09:11:32 +0200

    Update main.c

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=ese032-elencostudentisalvafile-noarray
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ese032-elencostudentisalvafile-noarray
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ese032-elencostudentisalvafile-noarray
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=ese032-elencostudentisalvafile-noarray.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ese032-elencostudentisalvafile-noarray.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ese032-elencostudentisalvafile-noarray.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
